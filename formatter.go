41ebb42f93227b5b00340887cdd1788db45ed1a9
gz-c <token@protonmail.com>
2017-10-04 19:12:03 +0800

    Fix double space in log line with empty message

--- formatter.go
+++ formatter.go
@@ -11,8 +11,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/sirupsen/logrus"
 	"github.com/mgutz/ansi"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
@@ -269,7 +269,11 @@ func (f *TextFormatter) printColored(b *bytes.Buffer, entry *logrus.Entry, keys 
 	}
 
 	if f.DisableTimestamp {
-		fmt.Fprintf(b, "%s%s "+messageFormat, level, prefix, message)
+		if message == "" {
+			fmt.Fprintf(b, "%s%s", level, prefix)
+		} else {
+			fmt.Fprintf(b, "%s%s "+messageFormat, level, prefix, message)
+		}
 	} else {
 		var timestamp string
 		if !f.FullTimestamp {
@@ -277,8 +281,16 @@ func (f *TextFormatter) printColored(b *bytes.Buffer, entry *logrus.Entry, keys 
 		} else {
 			timestamp = fmt.Sprintf("[%s]", entry.Time.Format(timestampFormat))
 		}
-		fmt.Fprintf(b, "%s %s%s "+messageFormat, colorScheme.TimestampColor(timestamp), level, prefix, message)
+
+		coloredTimestamp := colorScheme.TimestampColor(timestamp)
+
+		if message == "" {
+			fmt.Fprintf(b, "%s %s%s", coloredTimestamp, level, prefix)
+		} else {
+			fmt.Fprintf(b, "%s %s%s "+messageFormat, coloredTimestamp, level, prefix, message)
+		}
 	}
+
 	for _, k := range keys {
 		if k != "prefix" {
 			v := entry.Data[k]
